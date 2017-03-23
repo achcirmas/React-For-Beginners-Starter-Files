7c88e2c378715e324a608a9f50f3e63d35b378a5
Sam Rich <samrichca@gmail.com>
2017-03-22 21:40:33 -0400

    Video 4. Wrote some JSX and comments.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -5,7 +5,18 @@ import React from 'react';
 class StorePicker extends React.Component {
   // needs at least one method (render)
   render() {
-    return <p>Hello World</p>
+    // you can only return one parent element
+    // you must self-close tags like XHTML
+    // don't put comments at the top level in a return statement
+    return(
+      <form className="store-selector">
+        { /* this is how to write a comment */ }
+        <h2>Please enter a store.</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store →</button>
+
+      </form>
+      )
   }
 }
 
