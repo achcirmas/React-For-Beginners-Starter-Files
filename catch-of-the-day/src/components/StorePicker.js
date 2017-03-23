d9c783d03ff733a75b10437fcff7f79026681a18
Sam Rich <samrichca@gmail.com>
2017-03-22 21:22:57 -0400

    Videos 1-3. Created my first component, StorePicker.

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,12 @@
+// import react js
+import React from 'react';
+
+// make our first component
+class StorePicker extends React.Component {
+  // needs at least one method (render)
+  render() {
+    return <p>Hello World</p>
+  }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,9 @@
-// let's go!
+// import react js
+import React from 'react';
+// import render dom method. react isn't just for the web!
+import { render } from 'react-dom';
+
+// import the component
+import StorePicker from './components/StorePicker';
+// and put it in the dom somewhere
+render(<StorePicker/>, document.querySelector('#main'));
