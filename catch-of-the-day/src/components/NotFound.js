c223506a528dd626ea0019d311fe277632485c46
Sam Rich <samrichca@gmail.com>
2017-03-23 13:25:31 -0400

    Video 9. Routing with React Router

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  // needs at least one method (render)
+  render() {
+    return(
+      <p>NotFound</p>
+    )
+  }
+}
+
+export default NotFound;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,12 +2,32 @@
 import React from 'react';
 // import render dom method. react isn't just for the web!
 import { render } from 'react-dom';
+// import react router parts
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 // import the CSS file
 import './css/style.css';
 
-// import the app component
+// import the App component
 import App from './components/App';
-// import the component
+// import the StorePicker component
 import StorePicker from './components/StorePicker';
-// and put it in the dom somewhere
-render(<App/>, document.querySelector('#main'));
+// import the NotFound component
+import NotFound from './components/NotFound';
+
+// some people call this main or app
+// it's basically the main nav control
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match exactly pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+// this will call App or StorePicker
+render(<Root/>, document.querySelector('#main'));
