c026c7f01e1094df12f4a5d491c06b12020d8e38
Sam Rich <samrichca@gmail.com>
2017-03-23 13:13:27 -0400

    Video 7. Passing dynamic data with props.

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -4,7 +4,19 @@ class Header extends React.Component {
   // needs at least one method (render)
   render() {
     return(
-      <p>Header</p>
+      <header className="top">
+        <h1>
+        Catch 
+        <span className="ofThe">
+          <span className="of">of</span>
+          <span className="the">the</span>
+        </span>
+        Day
+        </h1>
+        <h3 class="tagline">
+          <span>{this.props.tagline}</span>
+        </h3>
+      </header>
     )
   }
 }
