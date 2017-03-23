22186165286107f1d438f885c3c42743d7b8e25e
Sam Rich <samrichca@gmail.com>
2017-03-23 13:16:41 -0400

    Video 8. Stateless functional components.

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
-class Header extends React.Component {
-  // needs at least one method (render)
-  render() {
-    return(
-      <header className="top">
-        <h1>
+// stateless functional component
+// could also be written
+// var Header = function() {}
+
+const Header = (props) => {
+  return (
+    <header className="top">
+      <h1>
         Catch 
         <span className="ofThe">
           <span className="of">of</span>
           <span className="the">the</span>
         </span>
         Day
-        </h1>
-        <h3 class="tagline">
-          <span>{this.props.tagline}</span>
-        </h3>
-      </header>
-    )
-  }
+      </h1>
+      <h3 class="tagline">
+        <span>{props.tagline}</span>
+      </h3>
+    </header>
+  )
 }
 
 export default Header;
