cbefab5e82bddd95da81ee98b1d1385f77e9f35e
Sam Rich <samrichca@gmail.com>
2017-03-23 20:07:10 -0400

    Video 11. Working with React events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,16 +4,31 @@ import { getFunName } from '../helpers';
 
 // make our first component
 class StorePicker extends React.Component {
+  // constructor() {
+  //   // super creates the react component
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  //   // looks for goToStore method and binds it to this
+  //   // this is a good idea if you're going to use a method more than once
+  // }
+
+  goToStore(event) {
+    event.preventDefault();
+    console.log("you changed the url");
+    // first grab the text from the box
+    console.log(this.storeInput.value);
+    // second we're going to transition from / to /store/storeId
+  }
   // needs at least one method (render)
   render() {
     // you can only return one parent element
     // you must self-close tags like XHTML
     // don't put comments at the top level in a return statement
     return(
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         { /* this is how to write a comment */ }
         <h2>Please enter a store.</h2>
-        <input defaultValue={getFunName()} type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store →</button>
       </form>
     )
